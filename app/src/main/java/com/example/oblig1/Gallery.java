f8aa7904eb902b5e277df1632f78e8d0d8e73820
Stian <89031614+h598993@users.noreply.github.com>
2024-02-07 19:35:16 +0100

    Added predefined images to global state and clean up

--- app/src/main/java/com/example/oblig1/Gallery.java
+++ app/src/main/java/com/example/oblig1/Gallery.java
@@ -12,42 +12,20 @@ import java.util.ArrayList;
 
 public class Gallery extends AppCompatActivity {
 
-
-    ArrayList<Item> items = new ArrayList<>();
-    int[] dogs = {R.drawable.bulldog,R.drawable.cockerspaniel,R.drawable.labrador};
-
-
-    ArrayList<Uri> dogImages = new ArrayList<>();
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MyApplication globalState = (MyApplication) getApplicationContext();
         setContentView(R.layout.activity_gallery);
         RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
-        setupItemModels();
 
-RecyclerViewAdapter adapter =  new RecyclerViewAdapter(this,items);
+
+RecyclerViewAdapter adapter =  new RecyclerViewAdapter(this,globalState.getItems());
 
 recyclerView.setAdapter(adapter);
 recyclerView.setLayoutManager(new LinearLayoutManager(this));
-MyApplication globalState = (MyApplication) getApplicationContext();
-    }
-
-
-    private void setupItemModels(){
-String[] dognames = new String[3];
-dognames[0] = "Bulldog";
-dognames[1] = "Cockerspaniel";
-dognames[2] = "Labrador";
-        dogImages.add(Uri.parse("android.resource://com.example.oblig1/drawable/bulldog"));
-        dogImages.add(Uri.parse("android.resource://com.example.oblig1/drawable/cockerspaniel"));
-        dogImages.add(Uri.parse("android.resource://com.example.oblig1/drawable/labrador"));
-for( int i = 0; i < dognames.length; i++){
-
-    items.add(new Item(dognames[i],dogImages.get(i)));
-}
-
-
 
     }
+
+
 }
--- app/src/main/java/com/example/oblig1/MyApplication.java
+++ app/src/main/java/com/example/oblig1/MyApplication.java
@@ -2,16 +2,18 @@ package com.example.oblig1;
 
 import android.app.Application;
 import android.content.ComponentCallbacks2;
+import android.net.Uri;
 
 import java.util.ArrayList;
 
 public class MyApplication extends Application implements ComponentCallbacks2 {
     ArrayList<Item> items = new ArrayList<>();
+    ArrayList<Uri> dogImages = new ArrayList<>();
     @Override
     public void onCreate() {
         super.onCreate();
 
-
+setupItemModels();
 
 
     }
@@ -23,4 +25,27 @@ public class MyApplication extends Application implements ComponentCallbacks2 {
     public ArrayList<Item> getItems() {
         return items;
     }
+
+
+
+    private void setupItemModels(){
+        String[] dognames = new String[3];
+        dognames[0] = "Bulldog";
+        dognames[1] = "Cockerspaniel";
+        dognames[2] = "Labrador";
+        dogImages.add(Uri.parse("android.resource://com.example.oblig1/drawable/bulldog"));
+        dogImages.add(Uri.parse("android.resource://com.example.oblig1/drawable/cockerspaniel"));
+        dogImages.add(Uri.parse("android.resource://com.example.oblig1/drawable/labrador"));
+        for( int i = 0; i < dognames.length; i++){
+
+            items.add(new Item(dognames[i],dogImages.get(i)));
+        }
+
+
+
+    }
+
+
+
+
 }
