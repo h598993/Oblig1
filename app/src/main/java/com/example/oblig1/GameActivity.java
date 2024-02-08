27c350afae1ef774ab096cc11ea7692ba57abb67
Stian <89031614+h598993@users.noreply.github.com>
2024-02-08 21:38:20 +0100

    Making variables global and moving logic to seperate method

--- app/src/main/java/com/example/oblig1/GameActivity.java
+++ app/src/main/java/com/example/oblig1/GameActivity.java
@@ -1,7 +1,9 @@
 package com.example.oblig1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,20 +20,127 @@ public class GameActivity extends AppCompatActivity {
 Random randomObjectToShow;
 Random randomPosition;
 int counter;
+int correctTextIndex;
+
+    ImageView image;
+    TextView optionOne;
+    TextView optionTwo;
+    TextView optionThree;
+    TextView counterView;
+
+    TextView answerResponse;
+
+    MyApplication globalState;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        MyApplication globalState = (MyApplication) getApplicationContext();
-        // Getting references
-        ImageView image = findViewById(R.id.image_game);
-        TextView optionOne = findViewById(R.id.option_one);
-        TextView optionTwo = findViewById(R.id.option_two);
-        TextView optionThree = findViewById(R.id.option_three);
-        TextView counterView = findViewById(R.id.text_score);
+
+
+        answerResponse = findViewById(R.id.answer_response);
 
         //Score counter
         counter = 0;
+
+       setupQuiz();
+
+
+        // setting up click events
+
+        optionOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(correctTextIndex == 0){
+                    counter++;
+                    counterView.setText(String.valueOf(counter));
+                    answerResponse.setText("Correct!");
+                    setupQuiz();
+                }else{
+
+                    answerResponse.setText("Wrong Ansver");
+                    setupQuiz();
+                }
+
+
+
+            }
+        });
+
+
+
+
+        optionTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(correctTextIndex == 1){
+                    counter++;
+                    counterView.setText(String.valueOf(counter));
+                    answerResponse.setText("Correct!");
+                    setupQuiz();
+                }else{
+
+                    answerResponse.setText("Wrong Ansver");
+                    setupQuiz();
+                }
+
+
+
+            }
+        });
+
+
+
+
+        optionThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(correctTextIndex == 2){
+                    counter++;
+                    counterView.setText(String.valueOf(counter));
+                    answerResponse.setText("Correct!");
+                    setupQuiz();
+                }else{
+
+                    answerResponse.setText("Wrong Ansver");
+                    setupQuiz();
+                }
+
+
+
+            }
+        });
+
+
+        Button backButton = findViewById(R.id.button_back_game);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(GameActivity.this, MainActivity.class);
+                startActivity(goBack);
+            }
+        });
+
+
+
+
+    }
+
+
+    private void setupQuiz (){
+
+        globalState = (MyApplication) getApplicationContext();
+
+
+        // Getting references
+        image = findViewById(R.id.image_game);
+        optionOne = findViewById(R.id.option_one);
+        optionTwo = findViewById(R.id.option_two);
+        optionThree = findViewById(R.id.option_three);
+        counterView = findViewById(R.id.text_score);
+
+
+
 
         // Storing list of strings to choose from in the quiz
         String[] randomDogNames = {"Puddel","Dalmantiner","Sjefer","Fuglehund","Rottveiler"};
@@ -45,19 +154,19 @@ int counter;
 
         //Setting up first picture
         Random r1 = new Random();
-       int randomObjectIndex = r1.nextInt(nrOfItems);
+        int randomObjectIndex = r1.nextInt(nrOfItems);
 
-       Item firstItem =  items.get(randomObjectIndex);
-       image.setImageURI(firstItem.getImageUri());
+        Item firstItem =  items.get(randomObjectIndex);
+        image.setImageURI(firstItem.getImageUri());
 
-       //setting up first text
+        //setting up first text
         Random r2 = new Random();
         String correctText = firstItem.getName();
-        int correctTextIndex = r2.nextInt(3);
+        correctTextIndex = r2.nextInt(3);
 
 
 
-
+    // really bad way of making sure to not pick the same name twice
         int firstPick = r2.nextInt(randomDogNames.length);
         int secondPick = r2.nextInt(randomDogNames.length);
         if(firstPick == secondPick){
@@ -68,6 +177,7 @@ int counter;
         }
 
 
+        //Binding the correct text to one of the three options then filling in the remaing with random names from the list
         switch (correctTextIndex){
 
             case 0:
@@ -97,73 +207,6 @@ int counter;
                 break;
 
         }
-
-
-
-        // setting up click events
-
-        optionOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(correctTextIndex == 0){
-                    counter++;
-                    counterView.setText(String.valueOf(counter));
-                }else{
-                    counter--;
-                    counterView.setText(String.valueOf(counter));
-                }
-
-
-
-            }
-        });
-
-
-
-
-        optionTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(correctTextIndex == 1){
-                    counter++;
-                    counterView.setText(String.valueOf(counter));
-                }else{
-                    counter--;
-                    counterView.setText(String.valueOf(counter));
-                }
-
-
-
-            }
-        });
-
-
-
-
-        optionThree.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(correctTextIndex == 2){
-                    counter++;
-                    counterView.setText(String.valueOf(counter));
-                }else{
-                    counter--;
-                    counterView.setText(String.valueOf(counter));
-                }
-
-
-
-            }
-        });
-
-
-
-
-    }
-
-
-    private void setupQuiz (){
-        
 
 
     }
